8cc539635220e7af1a9e6ffe1c7d8764e3a03562
Mockyy <32643968+Mockyy@users.noreply.github.com>
2020-11-02 15:14:48 +0100

    Fabrique de prefab

---
+++ Fabrique/Assets/Scripts/Capsule.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capsule : Fabrique
+{
+    //Script attaché au gameObject Capsule qui permet de l'instancier grâce à la méthode parent
+
+    public override void SpawnPrefab()
+    {
+        base.SpawnPrefab();
+    }
+}
---
+++ Fabrique/Assets/Scripts/Fabrique.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fabrique : MonoBehaviour
+{
+    //Le script de fabrique qui posséde la méthode virtuelle SpawnPrefab()
+
+    public virtual void SpawnPrefab()
+    {
+        Instantiate(this);
+    }
+}
